76d18b6d28d32a7eabd932c52898a5fc15bf347d
JanieTran <emerald1901@gmail.com>
2020-08-26 15:40:08 +0700

    Tutorial part 2

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -4,14 +4,43 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 from scrapy.item import Item, Field
+from scrapy.loader.processors import MapCompose, TakeFirst
+from datetime import datetime
+
+
+def remove_quotation_marks(text):
+    text = text.strip(u'\u201c'u'\u201d')
+    return text
+
+
+def convert_date(text):
+    return datetime.strptime(text, '%B %d, %Y')
+
+
+def parse_location(text):
+    return text[3:]
 
 
 class QuoteItem(Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    quote_content = Field()
+    quote_content = Field(
+        input_processor=MapCompose(remove_quotation_marks),
+        # Take the first value insted of a list of 1 value
+        output_processor=TakeFirst()
+    )
+    author_name = Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    author_birthday = Field(
+        input_processor=MapCompose(convert_date),
+        output_processor=TakeFirst()
+    )
+    author_born_location = Field(
+        input_processor=MapCompose(parse_location),
+        output_processor=TakeFirst()
+    )
+    author_bio = Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
     tags = Field()
-    author_name = Field()
-    author_birthday = Field()
-    author_born_location = Field()
-    author_bio = Field()
--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from tutorial.items import QuoteItem
 
 
 class QuotesSpider(scrapy.Spider):
@@ -10,25 +12,31 @@ class QuotesSpider(scrapy.Spider):
         quotes = response.css('div.quote')
 
         for quote in quotes:
-            yield {
-                'text': quote.css('.text::text').get(),
-                'author': quote.css('.author::text').get(),
-                'tags': quote.css('.tag::text').getall()
-            }
+            loader = ItemLoader(item=QuoteItem(), selector=quote)
+            loader.add_css(field_name='quote_content', css='.text::text')
+            loader.add_css(field_name='tags', css='.tag::text')
+            quote_item = loader.load_item()
 
             author_url = quote.css('.author + a::attr(href)').get()
             self.logger.info('Get author page url')
             # Go to author page
-            yield response.follow(author_url, callback=self.parse_author)
+            yield response.follow(
+                author_url,
+                callback=self.parse_author,
+                meta={'quote_item': quote_item}
+            )
 
         # Go to next page
         for a in response.css('li.next a'):
             yield response.follow(a, callback=self.parse)
 
     def parse_author(self, response):
-        yield {
-            'author_name': response.css('.author-title::text').get(),
-            'author_birthday': response.css('.author-born-date::text').get(),
-            'author_born_location': response.css('.author-born-location::text').get(),
-            'author_bio': response.css('.author-description::text').get()
-        }        
+        quote_item = response.meta['quote_item']
+        loader = ItemLoader(item=quote_item, response=response)
+
+        loader.add_css(field_name='author_name', css='.author-title::text')
+        loader.add_css(field_name='author_birthday', css='.author-born-date::text')
+        loader.add_css(field_name='author_born_location', css='.author-born-location::text')
+        loader.add_css(field_name='author_bio', css='.author-description::text')
+
+        yield loader.load_item()
