e70cd239e7c8f627467398468397e2264b0d6d33
JanieTran <emerald1901@gmail.com>
2020-08-27 18:21:41 +0700

    Parse business name, description, nearest cities

--- usa_veterinarians/usa_veterinarians/items.py
+++ usa_veterinarians/usa_veterinarians/items.py
@@ -1,13 +1,27 @@
 import scrapy
 from scrapy.item import Field
+from scrapy.loader.processors import TakeFirst, MapCompose
 
 
 class BusinessItem(scrapy.Item):
-    name = Field()
-    description = Field()
+    business_name = Field(
+        output_processor=TakeFirst()
+    )
+    state = Field(
+        output_processor=TakeFirst()
+    )
+    county = Field(
+        output_processor=TakeFirst()
+    )
+    city = Field(
+        output_processor=TakeFirst()
+    )
+    description = Field(
+        output_processor=TakeFirst()
+    )
+    nearest_cities = Field()
     highlights = Field()
     associatation = Field()
     serviced_areas = Field()
     contact = Field()
     hour_operation = Field()
-    nearest_cities = Field()
--- usa_veterinarians/usa_veterinarians/spiders/vet_business_spider.py
+++ usa_veterinarians/usa_veterinarians/spiders/vet_business_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from usa_veterinarians.items import *
 
 
 class BusinessSpider(scrapy.Spider):
@@ -6,19 +8,73 @@ class BusinessSpider(scrapy.Spider):
     start_urls = ['http://www.usa-veterinarians.com/']
 
     def parse(self, response):
-        state_urls = response.css('main > ul > li > a::attr(href)').getall()
-        state_names = response.css('main > ul > li > a::text').getall()
+        states = response.css('main > ul > li > a')
 
-        for url, state in zip(state_urls, state_names):
+        for state in states:
+            meta = {'state': state.css('a::text').get()}
             yield response.follow(
-                url,
+                state.css('a::attr(href)').get(),
                 callback=self.parse_county,
-                meta={'state': state}
+                meta=meta
             )
 
     def parse_county(self, response):
-        state = response.meta['state']
+        counties = response.css('main > h1 + p + div + h2 + ul + h2 + ul > li > a')
+        meta = {'state': response.meta['state']}
 
-        county_names = response.css('main > h1 + p + div + h2 + ul + h2 + ul > li > a::text').getall()
+        for county in counties:
+            meta['county'] = county.css('a::text').get()
 
-        yield {state: county_names}
+            yield response.follow(
+                county.css('a::attr(href)').get(),
+                callback=self.parse_city,
+                meta=meta
+            )
+
+    def parse_city(self, response):
+        cities = response.css('main > ul > li > a')
+        meta = {key: value for key, value in response.meta.items() if key in ['state', 'county']}
+
+        for city in cities:
+            meta['city'] = city.css('a::text').get()
+
+            yield response.follow(
+                city.css('a::attr(href)').get(),
+                callback=self.parse_business_list,
+                meta=meta
+            )
+
+    def parse_business_list(self, response):
+        business_list = response.css('.card a::attr(href)').getall()
+
+        for business in business_list:
+            yield response.follow(
+                business,
+                callback=self.parse_business,
+                meta=response.meta
+            )
+
+    def parse_business(self, response):
+        meta = response.meta
+        business_loader = ItemLoader(item=BusinessItem(), response=response)
+
+        # Location meta data
+        business_loader.add_value(field_name='state', value=meta['state'])
+        business_loader.add_value(field_name='county', value=meta['county'])
+        business_loader.add_value(field_name='city', value=meta['city'])
+
+        # Business name and description
+        business_loader.add_css(field_name='business_name', css='.item-heading span::text')
+        business_loader.add_css(field_name='description', css='.item-description p::text')
+
+        # Nearest cities
+        nearest_cities = []
+        nearest_cities_res = response.css('.aside-box ul li')
+        for city in nearest_cities_res:
+            name = city.css('a::text').get()
+            distance = city.css('span::text').get()
+            distance = distance.split('-')[1].strip()
+            nearest_cities.append({name: distance})
+        business_loader.add_value(field_name='nearest_cities', value=nearest_cities)
+
+        yield business_loader.load_item()
